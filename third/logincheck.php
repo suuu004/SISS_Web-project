409081e97d1980d2cd849be3104de56e4ea1499b
suuu004 <qkrdltn1216@naver.com>
2024-02-05 00:02:38 +0900

    5주차-웹프

---
+++ third/logincheck.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <?php
+        session_start();
+
+        $host = 'localhost';
+        $user = 'root';
+        $pw = '1111';
+        $dbName = 'privacy';
+        $mysqli = new mysqli($host, $user, $pw, $dbName);
+
+        $id = $_POST['email'];
+        $pw = $_POST['password'];
+
+        $query = "select * from member01 where member_email='$id' and member_password='$pw'";
+        $result = mysqli_query($mysqli, $query);
+        $row = mysqli_fetch_array($result);
+
+        if (empty($id) || empty($pw)) {
+            echo "<script>alert('아이디 또는 패스워드를 입력하여 주세요.');history.back();</script>";
+            exit;
+        } elseif ($id == $row['member_email'] && $pw == $row['member_password']) {
+            $_SESSION['id'] = $row['member_email'];
+            $_SESSION['name'] = $row['member_password'];
+
+            echo "<script>alert('로그인 성공');</script>";
+            echo "<script>location.href='home.php';</script>";
+        } else {
+            echo "<script>window.alert('invalid username or password');</script>";
+            echo "<script>location.href='log-in.html';</script>";
+        }
+    ?>
+</body>
+</html>
